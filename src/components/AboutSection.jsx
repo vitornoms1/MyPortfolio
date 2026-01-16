5ee15b78d6d750df5c6c129fd61ba8e2a28bb9cf
Vitor Noms Kuhn <vitornoms.kuhn@prolec.energy>
2026-01-16 14:09:42 -0300

    Finalizado

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -16,7 +16,7 @@ const AboutSection = () => {
         </h2>
 
         <p className="max-w-3xl mx-auto text-gray-400 text-lg leading-relaxed">
-          Developer Front End with solid experience in developing modern web applications, combining performance, scalability and usability. I am proficient in JavaScript, CSS and relational and non-relational databases, with practical experience in both the front-end and back-end. On the front-end, I focus on creating responsive, reusable and accessible interfaces; on the back-end, I develop robust and secure APIs. I am enthusiastic about best practices, versioning with Git and clean architecture. I am passionate about programming and I constantly seek to evolve my skills, exploring new technologies and improving the quality of the solutions I develop.
+          Developer Full Stack with solid experience in developing modern web applications, combining performance, scalability and usability. I am proficient in JavaScript, CSS and relational and non-relational databases, with practical experience in both the front-end and back-end. On the front-end, I focus on creating responsive, reusable and accessible interfaces; on the back-end, I develop robust and secure APIs. I am enthusiastic about best practices, versioning with Git and clean architecture. I am passionate about programming and I constantly seek to evolve my skills, exploring new technologies and improving the quality of the solutions I develop.
         </p>
       </motion.div>
 
