d761f0a73cf6550c21b561032b06632b0d7aa44f
Vitor Noms Kuhn <vitornoms.kuhn@prolec.energy>
2026-03-05 09:40:09 -0300

    New photo added

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import profileImage from '../assets/images/vitor.jpg'; 
+import profileImage from '../assets/images/vitor.jpeg'; 
 
 const HeroSection = () => {
   return (
